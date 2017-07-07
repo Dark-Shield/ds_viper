8869a66342edf8ea4d42b39b485f99028f3ca608
Wyste <wysterine@gmail.com>
2017-07-07 13:20:33 -0400

    Added Platoon Leader & Platoon Medic to soldiers

--- DarkShieldUnits/addons/cfgSoldiers.hpp
+++ DarkShieldUnits/addons/cfgSoldiers.hpp
@@ -22,6 +22,26 @@ class CfgVehicles
         hiddenSelectionsTextures[] = {"\a3\characters_f\common\data\basicbody_grey_co.paa"}; /// what texture does this soldier use for camo selection
         hideProxySelections[] = {"ghillie_hide"}; /// names of selections hidden on proxies (used in vest in this case, see overlaySelectionsInfo[])
     };
+    class DS_PlatoonLeader: DS_Soldier_base_F //Unit Class name: Class getting info from
+    {
+        scope = public; //viewable
+        backpack = ""; //Backpack
+        vehicleclass = "DS_Army"; //Unit Group
+        _generalMacro = "DS_PlatoonLeader"; //Class Name
+        displayName = "PLT Leader"; //In-Game Name
+        uniformClass = "VSM_Multicam_BDU_Camo"; //uniform you are using
+        camouflage = 0.8; //0.5 for Snipers | 1.0 for Man | 4 for Trucks | 8 for Tanks | 10 For Planes
+    };
+    class DS_PlatoonMedic: DS_Soldier_base_F //Unit Class name: Class getting info from
+    {
+        scope = public; //viewable
+        backpack = ""; //Backpack
+        vehicleclass = "DS_Army"; //Unit Group
+        _generalMacro = "DS_PlatoonMedic"; //Class Name
+        displayName = "PLT Medic"; //In-Game Name
+        uniformClass = "VSM_Multicam_Crye_Camo_SS"; //uniform you are using
+        camouflage = 0.8; //0.5 for Snipers | 1.0 for Man | 4 for Trucks | 8 for Tanks | 10 For Planes
+    };
     class DS_Rifleman: DS_Soldier_base_F //Unit Class name: Class getting info from
     {
         scope = public; //viewable
